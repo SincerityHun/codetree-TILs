323e001fdc2038606057bcba95aa3f1e99a8e16f
Jung Seonghun <mark.seonghun.jung@gmail.com>
2024-04-18 02:00:31 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 194ms, Memory: 26MB, Status: Passed - CodeTree

--- 240418/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240418/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,33 +1,43 @@
-n,m = map(int,input().split())
+n, m = map(int, input().split())
 
 matrix = []
 
 for i in range(n):
-    matrix.append(list(map(int,input().split()))) 
+    matrix.append(list(map(int, input().split())))
 
-start_index = (0,0)
-dr = [1,0]
-dc = [0,1]
+start_index = (0, 0)
+dr = [1, 0]
+dc = [0, 1]
 
 visited = set()
 visited.add(start_index)
-def in_range(cur_r,cur_c):
-    return (0<=cur_r<n)and(0<=cur_c<m)
+
+
+def in_range(cur_r, cur_c):
+    return (0 <= cur_r < n) and (0 <= cur_c < m)
+
+
+
+
+flag = False
 def dfs(cur_node):
+    global flag
     cur_r, cur_c = cur_node
-    if (cur_r,cur_c) == (n-1,m-1):
-        return True
+    if (cur_r, cur_c) == (n - 1, m - 1):
+        flag = True
+        return
+
     for i in range(2):
-        next_r,next_c = cur_r+dr[i], cur_c+dc[i]
-        if (next_r,next_c) in visited or not in_range(next_r,next_c):
+        next_r, next_c = cur_r + dr[i], cur_c + dc[i]
+        if ((next_r, next_c) in visited) or (not in_range(next_r, next_c)):
             continue
         if matrix[next_r][next_c] == 0:
             continue
-        visited.add((next_r,next_c))
-        return dfs((next_r,next_c))
-    return False
+        visited.add((next_r, next_c))
+        dfs((next_r, next_c))
+dfs(start_index)
 
-if dfs(start_index):
+if flag:
     print(1)
 else:
     print(0)
