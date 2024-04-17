1727c20123a56680ab05ce90c061a73feb500bee
Jung Seonghun <mark.seonghun.jung@gmail.com>
2024-04-18 01:45:51 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240418/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240418/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -13,10 +13,12 @@ visited = set()
 visited.add(start_index)
 def in_range(cur_r,cur_c):
     return (0<=cur_r<n)and(0<=cur_c<n)
+flag = False
 def dfs(cur_node):
     cur_r, cur_c = cur_node
     if (cur_r,cur_c) == (n-1,m-1):
-        return True
+        flag = True
+        return
     for i in range(2):
         next_r,next_c = cur_r+dr[i], cur_c+dc[i]
         if (next_r,next_c) in visited or not in_range(next_r,next_c):
@@ -24,10 +26,10 @@ def dfs(cur_node):
         if matrix[next_r][next_c] == 0:
             continue
         visited.add((next_r,next_c))
-        return dfs((next_r,next_c))
-    return False
-
-if dfs(start_index):
+        dfs((next_r,next_c))
+    return
+dfs(start_index)
+if flag:
     print(1)
 else:
     print(0)
