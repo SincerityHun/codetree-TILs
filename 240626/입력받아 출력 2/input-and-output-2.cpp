f3468c3182db4795d23cc6a20bb43658a912713e
Jung Seonghun <mark.seonghun.jung@gmail.com>
2024-06-26 19:57:58 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240626/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int first, second;
+    cin >> first;
+    cin.get();
+    cin >> second;
+    cout << first<<second;
+    return 0;
+}
