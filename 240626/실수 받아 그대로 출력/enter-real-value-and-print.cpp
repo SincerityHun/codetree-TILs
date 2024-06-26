74f2a490b1aee80d978d9d6ff1a5e85ac7d05a07
Jung Seonghun <mark.seonghun.jung@gmail.com>
2024-06-26 19:34:43 +0900

    Title: 실수 받아 그대로 출력, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240626/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double n;
+    cin >> n;
+    cout << fixed;
+    cout.precision(2);
+    cout << n << endl;
+    return 0;
+}
