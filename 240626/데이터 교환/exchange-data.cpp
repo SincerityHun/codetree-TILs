002ab9c8019fe10141d68695c206b846f27ef1ce
Jung Seonghun <mark.seonghun.jung@gmail.com>
2024-06-26 19:08:06 +0900

    Title: 데이터 교환, Time: 4ms, Memory: 0MB, Status: Passed - CodeTree

--- 240626/데이터 교환/exchange-data.cpp
+++ 240626/데이터 교환/exchange-data.cpp
@@ -4,7 +4,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a = 5, b = 6, c = 7;
     int temp = a;
-    a = b; b = c; c = temp;
+    a = c; c = b; b = temp; 
     cout << a << endl;
     cout << b << endl;
     cout << c << endl;
