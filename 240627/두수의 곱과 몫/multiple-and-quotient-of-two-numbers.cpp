9c41f3c2e9cb58a402015e83a16717ce0d740ad4
Jung Seonghun <mark.seonghun.jung@gmail.com>
2024-06-27 19:11:55 +0900

    Title: 두수의 곱과 몫, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240627/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int b , a;
+    cin >> b >> a;
+    cout << b << " * " << a << " = " << b*a << endl;
+    cout << b << " / " << a << " = " << b/a << endl;
+    return 0;
+}
