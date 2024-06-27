aa7a287728b0e7b2f3aa10177a9056904b4e4629
Jung Seonghun <mark.seonghun.jung@gmail.com>
2024-06-27 19:40:14 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240627/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    if (n >= 3000) cout << "book"<<endl;
+    else if(n>=1000) cout << "mask";
+    else cout << "no";
+    return 0;
+}
