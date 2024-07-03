1d6ce60a034f2cfe163b354c17db1e777e0f1909
Jung Seonghun <mark.seonghun.jung@gmail.com>
2024-07-03 15:08:39 +0900

    Title: 2의 거듭제곱수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240703/2의 거듭제곱수/pow-of-2.cpp
+++ 240703/2의 거듭제곱수/pow-of-2.cpp
@@ -4,9 +4,9 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int N,i = 1,temp = 2;
     cin >> N;
-    for(; i <= 10 && temp != N ; temp*=i,i++)
+    for(; i <= 10 && temp != N ; i++,temp*=i)
     {
     }
-    cout << (i-1) ;
+    cout << i ;
     return 0;
 }
