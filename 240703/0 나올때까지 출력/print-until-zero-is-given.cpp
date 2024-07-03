729b8a6726bd3d9f5ed1f2597b5bd348c6c1f3a8
Jung Seonghun <mark.seonghun.jung@gmail.com>
2024-07-03 14:56:34 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240703/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    while(true)
+    {
+        cin >> n;
+        if(!n) break;
+        cout << n << endl;
+    }
+    return 0;
+}
