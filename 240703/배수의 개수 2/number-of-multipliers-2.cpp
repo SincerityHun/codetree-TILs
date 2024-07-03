cb0cc5c18ada0d88e8895ba0112e2caee18a5a2a
Jung Seonghun <mark.seonghun.jung@gmail.com>
2024-07-03 14:03:20 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240703/배수의 개수 2/number-of-multipliers-2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int temp, count = 0;
+    for(int i = 0; i < 10; i++) (i%2)?count++:0;
+    cout << count;
+    return 0;
+}
