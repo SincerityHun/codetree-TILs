4ce8f44e67b0bc74d70deedc7c69b0c9e6a348d6
Jung Seonghun <mark.seonghun.jung@gmail.com>
2024-07-05 00:22:29 +0900

    Title: 규칙을 찾아 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240705/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
+++ 240705/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
@@ -4,23 +4,23 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n,limit_count;
     cin >> n;
-    for(int i = 0; i < n; i++) cout<<"*";
+    for(int i = 0; i < n; i++) cout<<"* ";
     cout << endl;
     if(n == 1) return 0;
     if(n == 2)
     {
-       for(int i = 0; i < n; i++) cout<<"*";
+       for(int i = 0; i < n; i++) cout<<"* ";
        cout << endl; 
        return 0;
     }
     limit_count = 1;
     for(int i = 0; i < n-2 ; i++)
     {
-        for(int j = 0; j < limit_count ; j++) cout <<"*";
-        for(int j = 0; j < n-limit_count-1 ; j++) cout <<" ";
-        cout << "*" << endl;    
+        for(int j = 0; j < limit_count ; j++) cout <<"* ";
+        for(int j = 0; j < n-limit_count-1 ; j++) cout <<"  ";
+        cout << "* " << endl;    
     }
-    for(int i = 0; i < n; i++) cout<<"*";
+    for(int i = 0; i < n; i++) cout<<"* ";
 
     return 0;
 }
