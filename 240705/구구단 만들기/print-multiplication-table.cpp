6f4753dffb643454a3719c59d69c6261d61679c8
Jung Seonghun <mark.seonghun.jung@gmail.com>
2024-07-05 01:42:25 +0900

    Title: 구구단 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240705/구구단 만들기/print-multiplication-table.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    for(int i = 1; i < 10; i++)
+    {
+        for(int j = b; j>=a; j-= 2 )
+        {
+            cout<<j<<" * "<<i<<" = "<<i*j;
+            if(j == a) break;
+            cout << " / ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
