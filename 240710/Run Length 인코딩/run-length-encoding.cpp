bb7d992dfcc6c8591f45d4d2c374cde95ecf5c48
Jung Seonghun <mark.seonghun.jung@gmail.com>
2024-07-10 17:28:15 +0900

    Title: Run Length 인코딩, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240710/Run Length 인코딩/run-length-encoding.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string A;
+    cin >> A;
+    char cur_item = A[0];
+    int arr[1000] = {},cnt = 0; //index,count
+    for(int i = 0 ; i < A.length(); i++)
+    {
+        // 현재꺼 가져오기
+        if(A[i] == cur_item) arr[cnt+1]++;
+        // 가져왔는데 같은 경우
+            // cnt+1 에 ++
+        else{
+            cur_item = A[i];
+            cnt += 2;
+            arr[cnt] = i;
+            arr[cnt+1]++;
+        }
+        // 가져왔는데 다른 경우
+            // cur_item 업데이트
+            // cnt 업데이트
+    }
+    cout << cnt +2 << endl;
+    for(int i = 0 ; i <= cnt; i+=2)
+    {
+        cout << A[arr[i]]<<arr[i+1];
+    }
+    return 0;
+}
