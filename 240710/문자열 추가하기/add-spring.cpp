1bc3453760b1256626c55eda00ac49520a56f7ce
Jung Seonghun <mark.seonghun.jung@gmail.com>
2024-07-10 17:36:15 +0900

    Title: 문자열 추가하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240710/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string input;
+    cin >> input;
+    cout << input+"Hello" ;
+    return 0;
+}
