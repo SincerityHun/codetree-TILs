0136dfa1c784dcaac15a0c18133d690a562921f1
Jung Seonghun <mark.seonghun.jung@gmail.com>
2024-07-10 16:56:32 +0900

    Title: 문자열 두번 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240710/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string test_case;
+    cin >> test_case;
+    cout << test_case <<endl;
+    cout << test_case <<endl;
+    return 0;
+}
