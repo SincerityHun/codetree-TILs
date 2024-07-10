d988fa0ac64225b9d9ef611cab91c881e707010b
Jung Seonghun <mark.seonghun.jung@gmail.com>
2024-07-10 16:57:49 +0900

    Title: 문자열의 길이 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240710/문자열의 길이 출력하기/print-string's-range.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string var1, var2;
+    cin >> var1 >> var2;
+    cout << var1.length() + var2.length();
+
+    return 0;
+}
