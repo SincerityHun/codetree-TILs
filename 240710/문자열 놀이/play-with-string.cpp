905cbdbdd703d2c054111c9e9fde339f30154af4
Jung Seonghun <mark.seonghun.jung@gmail.com>
2024-07-10 17:57:06 +0900

    Title: 문자열 놀이, Time: 1046ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240710/문자열 놀이/play-with-string.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string s;
+    int q;
+    cin >> s;
+    cin >> q;
+    for(int i = 0 ; i < q; i++)
+    {
+        int type;
+        cin >> type;
+        if(type == 1)
+        {
+            int a,b;
+            cin >> a >> b;
+            char temp = s[a-1];
+            s[a-1] = s[b-1];
+            s[b-1] = temp;
+        }
+        else{
+            char a,b;
+            cin >> a >> b;
+            int idx;
+            while((idx = s.find(a))!= string::npos)
+            {
+                s[idx] = b;
+            }
+        }
+        cout << s << endl;
+    }
+    return 0;
+}
